b696c6cea42937848c22d46ff69545d23af21193
Ravi soni <ets.raviindb@gmail.com>
2021-03-04 16:50:37 +0530

    dynamic_captcha_images_using_php

--- captcha.php
+++ captcha.php
@@ -1,12 +1,66 @@
-<?php 
-    $show_images = array(
-                        array("text" => "iMKiZ" ,"path" => "images/img1.png"),
-                        array("text" => "qGphJD","path" => "images/img2.jpg"),
-                        array("text" => "83tsU","path" => "images/img3.jpeg"),
-                        array("text" => "6138B","path" => "images/img4.jpeg"),
-                        array("text" => "PQJRYD","path" => "images/img6.gif"),
-                            );
-    $randomImage = rand(0, (count($show_images) - 1));
-        echo '<img src="' . $show_images[$randomImage]['path'] . '" width="150" height="60" class="captcha_view" id="captcha_view">';
-    $_SESSION["vercode"] = $show_images[$randomImage]['text']; 
-?>
+<?php
+session_start(); 
+$msg = ''; 
+
+if (isset($_POST['input_value']) > 0) 
+    // validation of captcha 
+    if ($_POST['input_value'] == $_SESSION['captcha']) 
+        $msg = '<span style="color:green">SUCCESSFUL !!!</span>'; 
+    else 
+        $msg = '<span style="color:red">CAPTCHA FAILED!!!</span>'; 
+?> 
+<style type="text/css">
+    <?php include 'css/generate_captcha.css'; ?>
+</style>
+<script>
+    function reload_captcha() {
+        // var xhttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        var xhttp =  new XMLHttpRequest();
+        xhttp.open("POST", "./show_captcha.php", true);//specifies the request
+        xhttp.send();//send the request to the server
+        // Defines a function to be called when the readyState property changes
+        xhttp.onreadystatechange = function()
+        {
+            // request finished and response is ready(4) & "OK"(200)
+            if (this.readyState == 4 && this.status == 200)
+            {
+                document.getElementsByClassName("captchaFront")[0].innerHTML = '<img src="./show_captcha.php" />';
+            }
+        }
+    }
+    window.load = reload_captcha();
+</script>
+<!Doctype html>
+<html>
+    <head>
+        <title>Custom Captcha Verification</title>
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <table>
+            <tr>
+                <td>
+                    <div style='margin-top:200px' class="captchaFront"></div><br><br>
+                    </div> 
+                </td>
+            </tr>
+
+            <tr>
+            <td class = "button_color">
+                <button type="button" class="inner_button" id="captcha_generator" name="refresh_button" onclick="return reload_captcha();">Refresh
+                        <i class="fa fa-refresh" style="font-size:24px"></i>
+                </button><br><br>
+                <div><?php echo $msg; ?></div> 
+            </td>
+            </tr>
+
+            <tr>
+            <td><input type="text" name="input_value" placeholder="Enter Verification code" required /></td>
+            <td><input type="submit" name="submit" value="Submit"/>
+            </tr>   
+        </table>
+        </form>
+    </body>
+</html> 
--- show_captcha.php
+++ show_captcha.php
@@ -1,19 +1,22 @@
 <?php 
 session_start(); 
 // Generate a random number 
-$captcha = rand(1000, 9999); 
+$captcha_text="abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ";
+$captcha_number ="123456789";
+$captcha_value = str_shuffle($captcha_text.$captcha_number);
+$captcha= substr($captcha_value, 0 ,7); 
 // The captcha value will be stored in the session 
 $_SESSION["captcha"] = $captcha; 
 // Generate a standard captcha image(width / height) 
-$captcha_image = imagecreatetruecolor(75, 45); 
+$captcha_image = imagecreatetruecolor(120, 45);  
 // Blue color 
-$bgColor = imagecolorallocate($captcha_image, 0, 120, 235); 
+$bgColor = imagecolorallocate($captcha_image, 255, 179, 179); 
 // White color 
 $textColor = imagecolorallocate($captcha_image, 13, 13, 13); 
 // Give the image a blue background 
 imagefill($captcha_image, 0, 0, $bgColor); 
 // Print the captcha text in the image
-imagestring($captcha_image, 5, 15, 15, $captcha, $textColor); 
+imagestring($captcha_image, 5, 30, 15, $captcha, $textColor); 
 //output the captcha as PNG image
 imagepng($captcha_image); 
 // It destroys the image and free memory 
